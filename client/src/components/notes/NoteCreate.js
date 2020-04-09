24bc62794d4393467cfedad9c1a86eb537bdc5cb
scrumpygeo <scrumpygeo@yahoo.ca>
2020-04-09 13:28:13 +0100

    add form validation

--- client/src/components/notes/NoteCreate.js
+++ client/src/components/notes/NoteCreate.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const NoteCreate = () => {
-  return <div>NoteCreate</div>;
+class NoteCreate extends Component {
+  renderInput({ input, label }) {
+    return (
+      <div className='form-group'>
+        <label>{label}</label>
+        <input className='form-control' {...input} />
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field name='title' component={this.renderInput} label='Enter Title' />
+        <Field
+          name='description'
+          component={this.renderInput}
+          label='Enter Description'
+        />
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
 };
 
-export default NoteCreate;
+export default reduxForm({
+  form: 'noteCreate',
+})(NoteCreate);

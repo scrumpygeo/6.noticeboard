0709cba73168c2076798d0724f6ebff7789efae2
scrumpygeo <scrumpygeo@yahoo.ca>
2020-04-10 11:52:38 +0100

    create and edit reducers

--- client/src/reducers/noteReducer.js
+++ client/src/reducers/noteReducer.js
@@ -10,6 +10,10 @@ export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_NOTE:
       return { ...state, [action.payload.id]: action.payload };
+    case CREATE_NOTE:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
     default:
       return state;
   }

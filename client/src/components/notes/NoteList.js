eafb69a186b5873a781871811213afe14a7178e0
scrumpygeo <scrumpygeo@yahoo.ca>
2020-04-10 14:09:59 +0100

    list notes

--- client/src/components/notes/NoteList.js
+++ client/src/components/notes/NoteList.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchNotes } from '../../actions';
 
-const NoteList = () => {
-  return <div>NoteList</div>;
+class NoteList extends Component {
+  componentDidMount() {
+    this.props.fetchNotes();
+  }
+
+  renderList() {
+    return this.props.notes.map((note) => {
+      return (
+        <div key={note.id} className='card my-2'>
+          <div className='card-header text-center'>{note.title}</div>
+          <div className='card-body'>
+            <i className='fas fa-flag mr-3' />
+            {note.description}
+          </div>
+          <div className='card-footer'>hello</div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className='text-center mb-3 py-2'>Notices</h2>
+        <div>{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { notes: Object.values(state.notes) };
 };
 
-export default NoteList;
+export default connect(mapStateToProps, { fetchNotes })(NoteList);

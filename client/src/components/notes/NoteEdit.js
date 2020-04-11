6e7c510579f180b5f2904c8a3922541e3fbf0c9d
scrumpygeo <scrumpygeo@yahoo.ca>
2020-04-11 16:30:46 +0100

    make edit note component get data independently

--- client/src/components/notes/NoteEdit.js
+++ client/src/components/notes/NoteEdit.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchNote } from '../../actions';
 
-const NoteEdit = () => {
-  return <div>NoteEdit</div>;
+class NoteEdit extends Component {
+  componentDidMount() {
+    this.props.fetchNote(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.note) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.note.title}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { note: state.notes[ownProps.match.params.id] };
 };
 
-export default NoteEdit;
+export default connect(mapStateToProps, { fetchNote })(NoteEdit);
